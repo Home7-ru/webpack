79593fdb4ee2ed5c022d067752ac11bafe18cec4
Ваше Имя <you@example.com>
2019-05-24 13:08:24 +0300

    добавлено jqwery и popper.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import $ from 'jquery';
+
+import "popper.js"
+
 import "./styles.css"
 let a=15;
+
+$(document).ready(function(){
+    $("a").click(function(event){
+        alert("As you can see, the link no longer took you to jquery.com");
+        event.preventDefault();
+    });
+});
 
 console.log("helo uord");
