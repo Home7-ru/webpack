71f6160cd47405df4424edf4221be4239890bfd7
andrey <you@example.com>
2019-05-31 08:58:11 +0300

    небольшие доделки в webservere

--- webpack.server.js
+++ webpack.server.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool:"source-map",
     devServer: {
         contentBase: './dist',
         compress: true,
